87fdf7c669eae1bbfd6445fa47bdfcf2e86da9b3
Omarshaltot <155308831+Omarshaltot@users.noreply.github.com>
2024-05-10 16:00:34 +0300

    studying gui

--- gui/src/App.java
+++ gui/src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        JLabel l = new JLabel();
+        JFrame f = new JFrame();
+        ImageIcon img = new ImageIcon("Screenshot 2024-04-23 000710.png");
+        img.getImage();
+        f.setVisible(true);
+        f.setSize(420,420);
+        l.setText("hello world");
+        l.setIcon(img);
+        l.setHorizontalTextPosition(JLabel.CENTER);
+        f.add(l);
     }
 }
